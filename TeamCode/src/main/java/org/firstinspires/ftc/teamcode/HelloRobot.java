c533136d14d091f45e8c25b8a68f2f1a43eb43e3
billbird02 <wjbird@gmail.com>
2026-03-11 15:31:27 -0400

    Hello Robot offseason program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloRobot.java
@@ -1,28 +1,23 @@
+/*
+Multi-line comments
+Program: Hello Robot
+ */
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous
-//@Disabled // comment this out to add to the OpMode list on the Driver Hub
 public class HelloRobot extends OpMode {
-    // this is a commen
-    //comment aslp
-    // another comment
-
-    /* comment
-    comment
-
-     */
 
     @Override
-    public void init() {    // this is the init method
+    public void init() {
+        telemetry.addLine("Hello Robot");
+        telemetry.update();     // update Driver Hub display
     }
 
     @Override
     public void loop() {
-        telemetry.addLine("Hello Robot");
-        telemetry.update();
+
     }
 }
